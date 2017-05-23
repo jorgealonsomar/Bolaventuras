f51a9118e3052f40024cf869b520d9c64df5ca83
Inter <Inteternenet-2@hotmail.es>
2017-05-23 20:10:19 +0200

    Añadido CSVLector. Lee un archivo CSV con separación de " entre columnas y \n entre filas.

---
+++ Assets/GameAssets/Scripts/Texto/CSVLector.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public static class CSVLector
+{
+
+    static string original;
+    static string[,] textos;
+
+    public static string ObtenerTexto(int indice)
+    {
+        if (textos == null)
+        {
+            if (original == null)
+            {
+                original = Resources.Load<TextAsset>("TestText").text;
+            }
+
+            string patrónColumnas = "\",\""; //string patrónFilas = "\n";
+            //Dividir en filas
+
+            //original = (original[0] == '"') ? original.Remove(0, 1) : original;
+            //original = (original[original.Length - 1] == '"') ? original.Remove(original.Length - 1, 1) : original;
+
+            string[] filas = original.Split("\n"[0]);
+            //filas = Regex.Split(original, patrónFilas);
+
+            int cantidadColumnas = 0;
+
+            for (int i = 0; i < filas.Length - 1; i++)
+            {
+                //Quitar las comillas
+                filas[i] = filas[i][0] == '"' ? filas[i].Remove(0, 1) : filas[i];
+                filas[i] = filas[i][filas[i].Length - 1] == '"' ? filas[i].Remove(filas[i].Length - 1) : filas[i];
+                string[] columnas = Regex.Split(filas[i], patrónColumnas); // filas[i].Split("\",\""[0]);
+
+                cantidadColumnas = Mathf.Max(cantidadColumnas, columnas.Length);
+            }
+
+            textos = new string[cantidadColumnas, filas.Length];
+            for (int y = 0; y < filas.Length - 1; y++)
+            {
+                string[] columna = Regex.Split(filas[y], patrónColumnas); //filas[y].Split("\",\""[0]);                
+                for (int x = 0; x < columna.Length; x++)
+                {
+                    textos[x, y] = columna[x];
+                }
+            }
+        }
+        return textos[0, indice];
+    }
+}
---
+++ Assets/GameAssets/Scripts/Texto/CSVTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSVTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(CSVLector.ObtenerTexto(1));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
