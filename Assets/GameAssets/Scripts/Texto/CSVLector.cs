82ddb88cb3dc3d6a4e0e985caea94fe1fd0b9e21
Inter <Inteternenet-2@hotmail.es>
2017-05-24 09:12:41 +0200

    Mejorado el lector CSV e implementado el mismo en el sistema de conversaciones

--- Assets/GameAssets/Scripts/Texto/CSVLector.cs
+++ Assets/GameAssets/Scripts/Texto/CSVLector.cs
@@ -6,49 +6,91 @@ using System.Text.RegularExpressions;
 public static class CSVLector
 {
 
-    static string original;
-    static string[,] textos;
+    static List<string[]> textos = new List<string[]>();
+
+    public enum Idioma { Español = 1, Ingles = 2 };
+    public static Idioma idioma = Idioma.Español;
 
     public static string ObtenerTexto(int indice)
     {
-        if (textos == null)
+        if (textos.Count == 0)
         {
-            if (original == null)
+            Inicializar();
+            if(textos.Count == 0)
             {
-                original = Resources.Load<TextAsset>("TestText").text;
-            }
-
-            string patrónColumnas = "\",\""; //string patrónFilas = "\n";
-            //Dividir en filas
-
-            //original = (original[0] == '"') ? original.Remove(0, 1) : original;
-            //original = (original[original.Length - 1] == '"') ? original.Remove(original.Length - 1, 1) : original;
-
-            string[] filas = original.Split("\n"[0]);
-            //filas = Regex.Split(original, patrónFilas);
-
-            int cantidadColumnas = 0;
-
-            for (int i = 0; i < filas.Length - 1; i++)
-            {
-                //Quitar las comillas
-                filas[i] = filas[i][0] == '"' ? filas[i].Remove(0, 1) : filas[i];
-                filas[i] = filas[i][filas[i].Length - 1] == '"' ? filas[i].Remove(filas[i].Length - 1) : filas[i];
-                string[] columnas = Regex.Split(filas[i], patrónColumnas); // filas[i].Split("\",\""[0]);
-
-                cantidadColumnas = Mathf.Max(cantidadColumnas, columnas.Length);
-            }
-
-            textos = new string[cantidadColumnas, filas.Length];
-            for (int y = 0; y < filas.Length - 1; y++)
-            {
-                string[] columna = Regex.Split(filas[y], patrónColumnas); //filas[y].Split("\",\""[0]);                
-                for (int x = 0; x < columna.Length; x++)
-                {
-                    textos[x, y] = columna[x];
-                }
+                Debug.LogWarning("Falta el archivo \"dialogos.csv\" en la carpeta Resources");
+                return "Este mensaje aparece porque no se ha encontrado la hoja de cálculo";
             }
         }
-        return textos[0, indice];
+        return textos[indice][(int)idioma];
+    }
+
+    public static LineaDeConversacion[] ObtenerLineasDeConversacion(int i)
+    {
+        Mathf.Clamp(--i, 0, int.MaxValue); //Para que el índice se corresponda con el número de fila en la tabla de cálculo
+
+        List<LineaDeConversacion> lineasDeConversacion = new List<LineaDeConversacion>();
+
+        while(i >= 0)
+        {
+            LineaDeConversacion nuevaLinea = new LineaDeConversacion();
+            nuevaLinea.texto = ObtenerTexto(i);
+
+            Dictionary<string, string> claves = new Dictionary<string, string>();
+            string[] datos = textos[i][0].Split(","[0]);
+
+            foreach (string dato in datos)
+            {
+                Debug.Log(dato);
+                if(dato.IndexOf(":") >= 0) claves.Add(dato.Split(":"[0])[0].ToLower(), dato.Split(":"[0])[1].ToLower());
+            }
+
+            string foto;
+            if (claves.TryGetValue("foto", out foto)) nuevaLinea.foto = Resources.Load<Sprite>("Fotos/" + foto);
+            else if (lineasDeConversacion.Count > 0) nuevaLinea.foto = lineasDeConversacion[lineasDeConversacion.Count - 1].foto;
+
+            string nombre;
+            if (claves.TryGetValue("nombre", out nombre)) nuevaLinea.nombre = nombre;
+            else if (lineasDeConversacion.Count > 0) nuevaLinea.nombre = lineasDeConversacion[lineasDeConversacion.Count - 1].nombre;
+
+            lineasDeConversacion.Add(nuevaLinea);
+
+            i = -1;
+            string siguiente;
+            if (claves.TryGetValue("siguiente", out siguiente)) i = EncontrarSiguiente(siguiente);
+        }
+
+        return lineasDeConversacion.ToArray();
+    }
+
+    static int EncontrarSiguiente(string siguiente)
+    {
+        int siguienteIndice = -1;
+        if (int.TryParse(siguiente, out siguienteIndice)) siguienteIndice--; //Para que el índice se corresponda con el número de fila en la tabla de cálculo
+        else
+        {
+            //???
+        }        
+        return siguienteIndice;
+    }
+
+    static void Inicializar()
+    {
+        string original = Resources.Load<TextAsset>("Conversaciones").text;
+        string[] filas = Regex.Split(original, "\n");
+
+        for (int y = 0; y < filas.Length; y++)
+        {
+            if (filas[y].Length > 0)
+            {
+                string fila = filas[y];
+
+                fila = fila.Remove(0, 1);
+                fila = fila.Remove(fila.LastIndexOf('\"'), 1);
+                fila = fila.Replace(@"\n", "\n");
+
+                textos.Add(Regex.Split(fila, "\",\""));
+            }
+        }
     }
 }
